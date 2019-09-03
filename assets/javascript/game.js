97746cab7112425b074bf37d523ea9eb34f6658a
rmjones112 <rmjones112gmail.com>
2019-09-02 19:44:22 -0700

    started javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+//game goal to match your score to random number at begining of game
+
+//need random number generator btw 19-120 for score you're trying to match 
+
+//need to create variables for gems, crystal #s randomly generated btw 1-12
+//need reset function at end
+
+//setting values for gemstones
+var gems = {
+    diamond:
+    {
+        name: "diamond",
+        value: 0
+    },
+
+    emerald:
+    {
+        name: "emerald",
+        value: 0
+    },
+    pink:
+    {
+        name: "pink",
+        value: 0
+    },
+    ruby:
+    {
+        name: "ruby",
+        value:0
+    }
+};
+
+//variables
+var win= 0;
+var lose= 0;
+var yourscore = 0;
+var goalscore = 0;
+
